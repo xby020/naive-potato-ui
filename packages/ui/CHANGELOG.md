351c98d640b1881f104d8d584b8af8f271019200
夏彬阳 <xby020@163.com>
2023-11-16 17:11:50 +0800

    feat(ui): add other child packages

--- packages/ui/CHANGELOG.md
+++ packages/ui/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @naive-potato-ui/ui
 
+## 1.0.9
+
+### Patch Changes
+
+- add other child packages.
+
 ## 1.0.8
 
 ### Patch Changes
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,3 @@
 export * from '@naive-potato-ui/curd-table';
+export * from '@naive-potato-ui/async-select';
+export * from '@naive-potato-ui/custom-upload';
